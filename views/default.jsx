6250a9a76d9ffd599c84b73d45543e03e9ab3e43
Carter Ervin <cjrox01@yahoo.com>
2021-12-06 20:04:08 -0600

    Added CSS link attempting to add dynamic JS to client

--- views/default.jsx
+++ views/default.jsx
@@ -4,6 +4,8 @@ function Def(html) {return (
     <html>
         <head>
             <title>Title</title>
+            {async () => {let bs = await fetch('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');eval(bs);console.log('Grabbed bs');}}
+            <link rel="stylesheet" href="/css/main.css" />
         </head>
         <body>
             {html.children}
